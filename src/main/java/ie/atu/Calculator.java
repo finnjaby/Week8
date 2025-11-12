65d21b0b179b711114f4f7ea5d70a28b3737db22
FinnjaBayer <g00470359@atu.ie>
2025-11-12 16:48:09 +0000

    added subtract method

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public int add(int firstNum, int secondNum) {
         return firstNum + secondNum;
     }
+
+    public int sub(int firstNum, int secondNum) {
+        return firstNum - secondNum;
+    }
 }
